db71ee41595144e653d2998e3a243c676af52014
tonmaz <tmazanhi@gmail.com>
2020-07-12 17:05:02 +0200

    first commint

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,24 +2,24 @@ import React, {forwardRef} from "react";
 import {Helmet} from "react-helmet";
 
 const Page = forwardRef(({title, children, ...rest}, ref) => {
-  // const location = useLocation();
+    // const location = useLocation();
 
-  // const sendPageViewEvent = useCallback(() => {
-  //   track.pageview({
-  //     page_path: location.pathname
-  //   });
-  // }, [location]);
-  //
-  // useEffect(() => {
-  //   sendPageViewEvent();
+    // const sendPageViewEvent = useCallback(() => {
+    //   track.pageview({
+    //     page_path: location.pathname
+    //   });
+    // }, [location]);
+    //
+    // useEffect(() => {
+    //   sendPageViewEvent();
   // }, [sendPageViewEvent]);
 
   return (
       <div ref={ref} {...rest}>
-        <Helmet>
-          <title>{title}</title>
-        </Helmet>
-        {children}
+          <Helmet>
+              <title>{title}</title>
+          </Helmet>
+          {children}
       </div>
   );
 });
